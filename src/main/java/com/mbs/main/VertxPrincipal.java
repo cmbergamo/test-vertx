c77f6c1c3ae6f3fe2e14ac73fe8f1dfedcd85259
Cristiano M Bergamo <cmbergamo@gmail.com>
2015-11-13 13:46:58 -0200

    Changes suggested by Michel;

--- src/main/java/com/mbs/main/VertxPrincipal.java
+++ src/main/java/com/mbs/main/VertxPrincipal.java
@@ -31,11 +31,8 @@ public class VertxPrincipal extends AbstractVerticle {
 
 		Router router = Router.router(vertx);
 
-		router.route().handler(BodyHandler.create());
-
 		SessionStore store = LocalSessionStore.create(vertx, "erpSession");
 
-		/*router.route().handler(CookieHandler.create());*/
 		SessionHandler sessionHandler = SessionHandler.create(store);
 		router.route().handler(sessionHandler);
 
@@ -53,35 +50,9 @@ public class VertxPrincipal extends AbstractVerticle {
 		EventBus eb = vertx.eventBus();
 		eb.registerCodec(new MbsUsuarioCodec());
 
-		router.route("/login.do").handler(_event -> {
-
-			/*JsonObject job = new JsonObject();
-
-			job.put("usuario", _event.request().getParam("usuario"));
-			job.put("senha", _event.request().getParam("senha"));
-
-			System.out.println(job);
-
-			eb.send("erp.autenticarcao", job, _resposta -> {
-
-				if (_resposta.succeeded()) {
-					_event.setUser((User) _resposta.result().body());
-					_event.next();
-				} else {
-					System.err.println(_resposta.cause());*/
-					_event.reroute("/dynamic/pageLogin.templ");
-				/*}
-
-			});*/
+		router.route("/login.do").handler(BodyHandler.create()).handler(_event -> {
+			_event.reroute("/dynamic/pageLogin.templ");
 		});
-
-		/*router.route("/login.do").handler(_event -> {
-			_event.response().putHeader("Content-type", "text/plain");
-			_event.response().setChunked(true);
-
-			System.out.println(_event.response().write("Finalizou com sucesso!!!"));
-			_event.response().end();
-		});*/
 
 		hs.requestHandler(router::accept).listen(8080);
 
